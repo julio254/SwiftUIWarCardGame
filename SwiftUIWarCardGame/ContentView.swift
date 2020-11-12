be1e5541de48a2a750d39f2f95e4133e6499381d
Julio Lopez <Juliol2540@gmail.com>
2020-11-11 19:55:54 -0600

    Initial Commit

---
+++ SwiftUIWarCardGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIWarCardGame
+//
+//  Created by Julio Lopez on 11/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIWarCardGame/SwiftUIWarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWarCardGameApp.swift
+//  SwiftUIWarCardGame
+//
+//  Created by Julio Lopez on 11/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
